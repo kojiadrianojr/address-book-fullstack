e981aa5584ced7256c4eb7f086c89ae0e3750555
Koji Adriano Jr <rolando.adriano@boom.camp>
2019-07-29 18:00:08 +0800

    set up backend, need to setup migrations

---
+++ database/server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const massive = require('massive');
+
+const app = express();
+
+
+const PORT = 3911
+app.listen(PORT, () => {console.log(`Server now listening d(-_-)b  @ ${PORT}`)});
+
